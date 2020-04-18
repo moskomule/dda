992150c95bf423952c374408bb33a67d0c5f7131
moskomule <hataya@keio.jp>
2020-04-18 11:54:19 +0900

    fix test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -4,26 +4,26 @@ import torch
 from dda import functional
 
 
-@pytest.mark.parametrize('f', [functional.shear_x,
-                               functional.shear_y,
-                               functional.translate_x,
-                               functional.translate_y,
-                               functional.rotate,
-                               functional.solarize,
-                               functional.posterize,
-                               functional.contrast,
-                               functional.saturate,
-                               functional.brightness,
-                               functional.hue,
-                               functional.sample_pairing,
-                               functional.sharpness,
-                               functional.gaussian_blur3x3]
+@pytest.mark.parametrize('f',
+                         [functional.shear_x,
+                          functional.shear_y,
+                          functional.translate_x,
+                          functional.translate_y,
+                          functional.rotate,
+                          functional.solarize,
+                          functional.posterize,
+                          functional.contrast,
+                          functional.saturate,
+                          functional.brightness,
+                          functional.hue,
+                          functional.sample_pairing,
+                          functional.sharpness,
+                          functional.gaussian_blur3x3]
                          )
-@pytest.mark.parametrize('i', [torch.randint(0, 255, (b, 3, 32, 32), dtype=torch.float32) / 255 for b in [1, 2, 4]])
-@pytest.mark.parametrize('v', [torch.rand(s) for s in [1, 2, 4]])
-def test_function_with_magnitude(f, i, v):
-    i = i.clone()
-    i.requires_grad_(True)
+@pytest.mark.parametrize('size', [1, 2, 4])
+def test_function_with_magnitude(f, size):
+    i = torch.randint(0, 255, (size, 3, 32, 32), dtype=torch.float32) / 255
+    v = torch.rand(size, requires_grad=True)
     f(i, v).mean().backward()
 
 
