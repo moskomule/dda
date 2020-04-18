9bacdbd4800aa85108b1dc92eda4cfeefef5abae
moskomule <hataya@keio.jp>
2020-04-18 11:03:58 +0900

    fix test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -4,55 +4,37 @@ import torch
 from dda import functional
 
 
-@pytest.fixture
-def inputs():
-    return [torch.randint(0, 255, (b, 3, 32, 32), dtype=torch.float32) / 255 for b in [1, 2, 4]]
+@pytest.mark.parametrize('f', [functional.shear_x,
+                               functional.shear_y,
+                               functional.translate_x,
+                               functional.translate_y,
+                               functional.rotate,
+                               functional.solarize,
+                               functional.posterize,
+                               functional.contrast,
+                               functional.saturate,
+                               functional.brightness,
+                               functional.hue,
+                               functional.sample_pairing,
+                               functional.sharpness,
+                               functional.gaussian_blur3x3]
+                         )
+@pytest.mark.parametrize('i', [torch.randint(0, 255, (b, 3, 32, 32), dtype=torch.float32) / 255 for b in [1, 2, 4]])
+@pytest.mark.parametrize('v', [torch.rand(s, requires_grad=True) for s in [1, 2, 4]])
+def test_function_with_magnitude(f, i, v):
+    try:
+        f(i, v).mean().backward()
+    except RuntimeError as e:
+        print(f'function {f.__name__} causes an error')
+        raise e
 
 
-@pytest.fixture
-def vs():
-    return [torch.rand(s, requires_grad=True) for s in [1, 2, 4]]
-
-
-@pytest.fixture
-def fs_with_mag():
-    return [functional.shear_x,
-            functional.shear_y,
-            functional.translate_x,
-            functional.translate_y,
-            functional.rotate,
-            functional.solarize,
-            functional.posterize,
-            functional.contrast,
-            functional.saturate,
-            functional.brightness,
-            functional.hue,
-            functional.sample_pairing,
-            functional.sharpness,
-            functional.gaussian_blur3x3]
-
-
-@pytest.fixture
-def fs_without_mag():
-    return [functional.vflip,
-            functional.hflip,
-            functional.invert,
-            functional.gray,
-            functional.auto_contrast,
-            functional.equalize]
-
-
-def test_function_with_magnitude(fs_with_mag, inputs, vs):
-    for f in fs_with_mag:
-        for input, v in zip(inputs, vs):
-            try:
-                f(input, v).mean().backward()
-            except RuntimeError as e:
-                print(f'function {f.__name__} causes an error')
-                raise e
-
-
-def test_function_without_magnitude(fs_without_mag, inputs):
-    for f in fs_without_mag:
-        for input in inputs:
-            f(input, None)
+@pytest.mark.parametrize('f', [functional.vflip,
+                               functional.hflip,
+                               functional.invert,
+                               functional.gray,
+                               functional.auto_contrast,
+                               functional.equalize])
+@pytest.mark.parametrize('i', [torch.randint(0, 255, (b, 3, 32, 32), dtype=torch.float32) / 255 for b in [1, 2, 4]])
+def test_function_without_magnitude(f, i):
+    f(i, None)
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,29 +4,24 @@ import torch
 from dda import operations
 
 
-@pytest.fixture
-def inputs():
-    return [torch.randint(0, 255, (b, 3, 32, 32), dtype=torch.float32) / 255 for b in [1, 2, 4]]
-
-
-def test_operations(inputs):
-    for module in [operations.ShearX(),
-                   operations.ShearY(),
-                   operations.TranslateY(),
-                   operations.TranslateY(),
-                   operations.Rotate(),
-                   operations.Invert(),
-                   operations.HorizontalFlip(),
-                   operations.Invert(),
-                   operations.Solarize(),
-                   operations.Posterize(),
-                   operations.Contrast(),
-                   operations.Saturate(),
-                   operations.Brightness(),
-                   operations.Sharpness(),
-                   operations.AutoContrast(),
-                   operations.Equalize(),
-                   operations.SamplePairing()]:
-        for input in inputs:
-            out = module(input)
-            out.mean().backward()
+@pytest.mark.parametrize('op', [operations.ShearX(),
+                                operations.ShearY(),
+                                operations.TranslateY(),
+                                operations.TranslateY(),
+                                operations.Rotate(),
+                                operations.Invert(),
+                                operations.HorizontalFlip(),
+                                operations.Invert(),
+                                operations.Solarize(),
+                                operations.Posterize(),
+                                operations.Contrast(),
+                                operations.Saturate(),
+                                operations.Brightness(),
+                                operations.Sharpness(),
+                                operations.AutoContrast(),
+                                operations.Equalize(),
+                                operations.SamplePairing()])
+@pytest.mark.parametrize('input', [torch.randint(0, 255, (b, 3, 32, 32), dtype=torch.float32) / 255 for b in [1, 2, 4]])
+def test_operations(op, input):
+    out = op(input)
+    out.mean().backward()
